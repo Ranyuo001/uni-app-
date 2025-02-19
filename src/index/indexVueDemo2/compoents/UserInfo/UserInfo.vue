0bb2aebdc68882ce8ff29cf91004784d2314a6ab
ranyuo <ranyuo002@163.com>
2025-02-19 21:19:01 +0800

    学习了props传参，并且发现静态资源必须存储在根目录文件夹中才能编译成功，否则会报错。

--- src/index/indexVueDemo2/compoents/UserInfo/UserInfo.vue
+++ src/index/indexVueDemo2/compoents/UserInfo/UserInfo.vue
@@ -1,12 +1,41 @@
 <template>
-  <view>
-    <image src="../../static/logo.png" mode="scaleToFill" />
-    <view class="username">张三</view>
+  <view class="userinfo">
+    <!-- <image :src="avatar" mode="" class="avatar"></image> -->
+    <image :src="avatar" mode="" class="avatar"></image>
+    <view class="username">{{ username }}</view>
   </view>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import { computed, ref } from "vue"
+const props = defineProps(['username', 'avatar'])
+// const myname = computed(() => {
+//   return props.username
+// })
+console.log(props);
+
 
 </script>
 
-<style scoped lang="scss"></style>
+<style lang="scss" scoped>
+.userinfo {
+  width: 100%;
+  height: 200px;
+  background: #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+
+  .username {
+    padding: 10px 0;
+    font-size: 20px;
+  }
+}
+</style>
--- src/index/indexVueDemo2/pages/demo1/demo1.vue
+++ src/index/indexVueDemo2/pages/demo1/demo1.vue
@@ -1,13 +1,18 @@
 <template>
   <view class="content">
-    <UserInfo></UserInfo>
-    <xxm_header></xxm_header>
+    <UserInfo :username="xxm" avatar="../../../../static/pic1.jpg"></UserInfo>
+    <UserInfo avatar="../../../../static/pic2.jpg"></UserInfo>
+    <UserInfo :username="Zhangsan" avatar="../../../../static/pic3.jpg"></UserInfo>
   </view>
 </template>
 
 <script setup lang="ts">
 import UserInfo from '../../compoents/UserInfo/UserInfo.vue'
-import xxm_header from '../../compoents/xxm_header/xxm_header.vue'
-</script>
+// import xxm_header from '../../compoents/xxm_header/xxm_header.vue'
+import { ref } from 'vue'
+const xxm = ref('xxm')
+const Lisi = ref('李四')
+const Zhangsan = ref('张三')
 
+</script>
 <style lang="scss" scoped></style>
